5f195d05aa2e1d0bd9315d2cb4b47ff14c399e05
Theresa Barkasy <theresa.barkasy@gmail.com>
2024-01-31 13:33:29 -0500

    sections and css

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,32 @@
-<?php 
-    echo "Hello Admin"
-?>
+<html>
+    <head>
+        <title>Quantum Eats - Home Page</title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+    <body>
+        <h1>Admin Panel</h1>
+        <!-- Menu Section Starts -->
+        <section>
+            <div class="menu">
+                <div class="wrapper">
+                    Menu Section Here
+                </div>
+            </div>
+        </section>
+        <!-- Menu Section Ends -->
+        <!-- Main Content Section Starts -->
+        <main>
+            <div class="main">
+                Main Section Here
+            </div>
+        </main>
+        <!-- Main Content Section Ends -->
+        <!-- Footer Section Starts -->
+        <footer>
+            <div class="footer">
+                Footer Section Here
+            </div>
+        </footer>
+        <!-- Footer Section Ends -->
+    </body>
+</html>
