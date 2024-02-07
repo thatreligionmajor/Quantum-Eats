6f4fb04c6378fdf7fd67711d517ad32606fbfd77
Theresa Barkasy <theresa.barkasy@gmail.com>
2024-02-07 16:31:17 -0500

    manage-admin.php

---
+++ admin/manage-admin.php
@@ -0,0 +1,70 @@
+<html>
+    <head>
+        <title>Quantum Eats - Home Page</title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+    <body>
+        <!-- <h1>Admin Panel</h1> -->
+        <!-- Menu Section Starts -->
+        <section>
+            <div class="menu text-center">
+                <div class="wrapper">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Admin Manager</a></li>
+                        <li><a href="#">Category</a></li>
+                        <li><a href="#">Food</a></li>
+                        <li><a href="#">Order</a></li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <!-- Menu Section Ends -->
+        <!-- Main Content Section Starts -->
+        <section>
+            <main>
+                <div class="main">
+                    <div class="wrapper">
+                        <h1>Dashboard</h1>
+                        <div class="row">
+                            <div class="col-4 text-center">
+                                <h2>5</h2>
+                                    </br>
+                                Categories
+                            </div>
+                            <div class="col-4 text-center">
+                                <h2>5</h2>
+                                    </br>
+                                Categories
+                            </div>
+                            <div class="col-4 text-center">
+                                <h2>5</h2>
+                                    </br>
+                                Categories
+                            </div>
+                            <div class="col-4 text-center">
+                                <h2>5</h2>
+                                    </br>
+                                Categories
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </section>
+            <!-- Main Content Section Ends -->
+            <!-- Footer Section Starts -->
+            <footer>
+                <div class="footer">
+                <div class="wrapper">
+                    <p class="text-center"> 
+                        <?php 
+                            echo "© " . date("Y") . " - All Rights Reserved, Quantum Eats. Developed by - <a href='https://github.com/thatreligionmajor'>Theresa Barkasy</a>";
+                        ?>
+                    </p>
+                 </div>
+            </div>
+        </footer>
+        <!-- Footer Section Ends -->
+    </body>
+</html>
