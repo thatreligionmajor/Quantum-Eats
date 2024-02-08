e896b8a5cb7e58f9a9f3821819c4c33db9044416
Theresa Barkasy <theresa.barkasy@gmail.com>
2024-02-07 23:01:59 -0500

    admin table

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -5,9 +5,43 @@
         <div class="main">
             <div class="wrapper">
                 <h1>Admin Manager</h1>
-                <div class="row text-center">
-                    
-                </div>
+                <!-- Button to Add Admin -->
+                <a href="#" class ="btn-primary btn-top">Add Admin</a>
+                <table class="tbl-full">
+                    <tr>
+                        <th>Serial Number</th>
+                        <th>Full Name</th>
+                        <th>Username</th>
+                        <th>Actions</th>
+                    </tr>
+                    <tr>
+                        <td>1. </td>
+                        <td>Your Name</td>
+                        <td>yourusername</td>
+                        <td>
+                            Update Admin
+                            Delete Admin
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>2. </td>
+                        <td>Your Name</td>
+                        <td>yourusername</td>
+                        <td>
+                            Update Admin
+                            Delete Admin
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>3. </td>
+                        <td>Your Name</td>
+                        <td>yourusername</td>
+                        <td>
+                            Update Admin
+                            Delete Admin
+                        </td>
+                    </tr>
+                </table>
             </div>
         </div>
     </main>
