9d5b71f3f2c0267107f726c4ea1b131a9139accf
Theresa Barkasy <theresa.barkasy@gmail.com>
2024-02-09 10:28:56 -0500

    added add-admin.php and started form

---
+++ admin/add-admin.php
@@ -0,0 +1,30 @@
+<?php include('components/navbar.php');?>
+<!-- Main Content Section Starts -->
+<section>
+    <main>
+        <div class="main">
+            <div class="wrapper">
+                <div class="header">
+                    <h1>Add Admin</h1>
+                    <!-- Button to Add Admin -->
+                    <br />
+                    <br />
+                    <br />
+                    <!-- <a href="add-admin.php" class ="button btn-primary">Add Admin</a> -->
+                    <br />
+                    <form action="" method="POST" >
+                        <table>
+                            <tr>
+                                <td>Full Name: </td>
+                                <td><input type="text" name="full_name" placeholder="enter your name"></td>
+                            </tr>
+                        </table>
+                    </form>
+                </div>
+                
+            </div>
+        </div>
+    </main>
+</section>
+
+<?php include('components/footer.php');?>
--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -10,7 +10,7 @@
                     <br />
                     <br />
                     <br />
-                    <a href="#" class ="button btn-primary">Add Admin</a>
+                    <a href="add-admin.php" class ="button btn-primary">Add Admin</a>
                     <br />
                 </div>
                 <table class="tbl-full">
