4c66a997134c92ce4b2ca481b36f8030dfb7f11d
Theresa Barkasy <theresa.barkasy@gmail.com>
2024-02-15 11:51:38 -0500

    display name and username in edit form

--- admin/update-admin.php
+++ admin/update-admin.php
@@ -32,10 +32,14 @@
                             //check whether we have admin data or not
                             if($count==1) {
                                 //details
+                                // echo "Admin Available";
+                                $row=mysqli_fetch_assoc($res);
+                                $full_name = $row['full_name'];
+                                $username = $row['username'];
                             }
                             else {
                                 //redirect to manage admin page
-                                header('location:'. SITEURL.'admin/manage-admin.php');
+                                header('location:'.SITEURL.'admin/manage-admin.php');
                             }
                         }
                     ?>
@@ -48,13 +52,13 @@
                             <tr>
                                 <td>Full Name: </td>
                                 <td>
-                                    <input type="text" name="full_name" value="">
+                                    <input type="text" name="full_name" value="<?php echo $full_name; ?>">
                                 </td>
                             </tr>
                             <tr>
                                 <td>Username: </td>
                                 <td>
-                                    <input type="text" name="username" value="">
+                                    <input type="text" name="username" value="<?php echo $username; ?>">
                                 </td>
                             </tr>
                             <tr>
