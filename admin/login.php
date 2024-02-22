73d67a92629e98638b5fad4b47fc1637a9f4ec88
Theresa Barkasy <theresa.barkasy@gmail.com>
2024-02-21 20:44:04 -0500

    Pretty Form Inputs

--- admin/login.php
+++ admin/login.php
@@ -3,15 +3,23 @@
 <section>
     <main>
         <div class="main">
-            <div class="wrapper login text-center">
+            <div class="wrapper login text-center rounded">
                 <h1>Login</h1>
+                <br>
                 <!-- Login Form Starts Here -->
                 <form action="" method="POST">
-                    Username: <br>
-                    <input type="text" name="username" placeholder="Enter your username"><br>
-                    Password:<br>
-                    <input type="password" name="password" placeholder="Enter your password"><br>
-                    <input type="submit" name="submit" value="Login" class="button btn-primary"><br>
+                    <div class="inputContainer">
+                        <input type="text" name="username" placeholder="Enter your username" class="input">
+                        <label for="" class="label">Username</label>
+                    </div>
+                    <!-- <br> -->
+                    <div class="inputContainer">
+                        <input type="password" name="password" placeholder="Enter your password" class="input">
+                        <label for="" class="label">Password</label>
+                    </div>
+                    <!-- <br> -->
+                    <input type="submit" name="submit" value="Login" class="button btn-primary">
+                    <br>
                 </form>
                 <!-- Login Form Ends Here -->
             </div>
