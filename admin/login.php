1086da4a4a6571d972579d2041f23f353c31b223
Theresa Barkasy <theresa.barkasy@gmail.com>
2024-02-24 19:49:50 -0500

    login in progress

--- admin/login.php
+++ admin/login.php
@@ -6,6 +6,14 @@
             <div class="wrapper login text-center rounded">
                 <h1>Login</h1>
                 <br>
+                <?php
+                    if(isset($_SESSION['login']))
+                    {
+                        echo $_SESSION['login'];
+                        unset ($_SESSION['login']);
+                    }
+                ?>
+                <br>
                 <!-- Login Form Starts Here -->
                 <form action="" method="POST">
                     <div class="inputContainer">
@@ -28,3 +36,34 @@
 </section>
 <!-- Main Content Section Ends -->
 <?php include('components/footer.php');?>
+
+<?php
+    // check whether the button is clicked
+    if(isset($_POST['submit']))
+    {
+        // login process
+        //1. get the data from the login form
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        //2. sql query to check whether the user with username and password exists
+        $sql = "SELECT * FROM tbl_admin WHERE username='$username' AND password='$password'";
+        //3. execute the sql query
+        $res = mysqli_query($conn, $sql);
+        //4. count the rows to check whether the user exists
+        $count = mysqli_num_rows($res);
+        if($count==1)
+        {
+            //user available and login successful
+            $_SESSION['login'] = "<div class='success'>Login Successful</div>";
+            //redirect to the home page
+            header('location:'.SITEURL.'admin/');
+        }
+        else{
+            //user not available and login failed
+            $_SESSION['login'] = "<div class='error text-center'>username or password did not match</div>";
+            //redirect to the home page
+            header('location:'.SITEURL.'admin/login.php');
+        }
+
+    }
+?>
