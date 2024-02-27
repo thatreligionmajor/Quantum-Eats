1a20acc25189cde46ed918a5d56e7a9c32a12505
Theresa Barkasy <theresa.barkasy@gmail.com>
2024-02-27 10:15:22 -0500

    skeleton for add-category.php

---
+++ admin/components/add-category.php
@@ -0,0 +1,30 @@
+<?php include('components/menu.php'); ?>
+<!-- Main Content Section Starts -->
+<section>
+    <main>
+        <div class="main">
+            <div class="wrapper">
+                <div class="header">
+                    <h1>Add Category</h1>
+                    <!-- Button to Add Category -->
+                    <br />
+                    <?php 
+                        //ifelse
+                    ?>
+                    <br />
+                    <br />
+                    <br />
+                    <!--  -->
+                    <br />
+                    <form action="" method="POST" >
+                        <table class="tbl-30">
+                            
+                        </table>
+                    </form>
+                </div>
+                
+            </div>
+        </div>
+    </main>
+</section>
+<?php include('components/footer.php'); ?>
