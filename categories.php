c1fb4bbe5910165a66ac0b221d47fe41bae6c0ac
Theresa Barkasy <theresa.barkasy@gmail.com>
2024-04-03 09:33:18 -0400

    bones for categories page

---
+++ categories.php
@@ -0,0 +1,31 @@
+<?php include('components-front/navbar.php'); ?>
+
+<!-- Categories  Start -->
+<section class="categories">
+        <div class="container">
+            <h2 class="section-header text-center">Molecular Moments</h2>
+            <div class="row blur">
+                <a href="#" class="linkrow">
+                    <div class="catcard shadow bigger">
+                            <h3 class="card-title">Breakfast</h3>
+                            <img src="images/molecular-benedict-spheres.jpg" alt="breakfast" class="img-responsive cat-img" />
+                    </div>
+                </a>
+                <a href="#" class="linkrow" >
+                    <div class="catcard shadow bigger">
+                        <h3 class="card-title">Lunch</h3>
+                        <img src="images/quinoa-fusion-salad-cloud.jpg" alt="lunch" class="img-responsive cat-img" />
+                    </div>
+                </a>
+                <a href="#" class="linkrow" >
+                    <div class="catcard shadow bigger">
+                        <h3 class="card-title">Dinner</h3>
+                        <img src="images/galactic-gourmet-steak.jpg" alt="dinner" class="img-responsive cat-img" />
+                    </div>
+                </a>
+            </div>
+        </div>
+    </section>
+    <!-- Categories End -->
+
+<?php include('partials-front/footer.php'); ?>
