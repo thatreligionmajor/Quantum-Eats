319c61460a6b3ed4096a7f637b5629d3e239ce47
Theresa Barkasy <theresa.barkasy@gmail.com>
2024-04-08 21:10:40 -0400

    bones for contact page

--- contact.php
+++ contact.php
@@ -4,7 +4,14 @@
         <div class="container">
             <h2 class="section-header menu-header text-center">Contact</h2>
             <div class="row">
-
+                <div>
+                    <div>Phone</div>
+                    <div>Email</div>
+                </div>
+                <div>
+                    <div>Address</div>
+                    <div>Hours</div>
+                </div>
             </div>
         </div>
 </section>
