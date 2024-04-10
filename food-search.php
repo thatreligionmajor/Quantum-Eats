c9fb5966d471c08b885370d5dd128070ef58da3c
Theresa Barkasy <theresa.barkasy@gmail.com>
2024-04-09 20:36:38 -0400

    food-search started

--- food-search.php
+++ food-search.php
@@ -1,5 +1,14 @@
 <?php include('components-front/navbar.php'); ?>
 
-<div>Food Search</div>
+<!--  Search Start -->
+<section class="search text-center">
+        <div class="container">
+            <form action="">
+                <input type="search" name="search" id="search" placeholder="what are you craving?">
+                <input type="submit" value="Search" class="search-button button-primary button" />
+            </form>
+        </div>
+    </section>
+    <!-- Search End -->
 
 <?php include('components-front/footer.php'); ?>
