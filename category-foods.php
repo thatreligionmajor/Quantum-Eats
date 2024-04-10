0a861176ed9e135f6d60422e2be9fad865814d91
Theresa Barkasy <theresa.barkasy@gmail.com>
2024-04-10 15:16:20 -0400

    starting category-foods sections

--- category-foods.php
+++ category-foods.php
@@ -2,4 +2,12 @@
 
 <div>Category Foods</div>
 
+<!-- Food Search Starts Here -->
+
+<!-- Food Search Ends Here -->
+
+<!-- Menu Section Starts Here -->
+
+<!-- Menu Section Ends Here -->
+
 <?php include('components-front/footer.php'); ?>
