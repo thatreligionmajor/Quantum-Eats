b472dfcdcd83873f6b881c3069871828e4075108
Theresa Barkasy <theresa.barkasy@gmail.com>
2024-04-15 17:05:49 -0400

    active categories displaying from the database on categories.php

--- categories.php
+++ categories.php
@@ -5,13 +5,52 @@
         <div class="container">
             <h2 class="section-header text-center">Molecular Moments</h2>
             <div class="row blur">
-                <a href="#" class="linkrow">
-                    <div class="catcard shadow bigger">
-                            <h3 class="card-title">Breakfast</h3>
-                            <img src="images/molecular-benedict-spheres.jpg" alt="breakfast" class="img-responsive cat-img" />
-                    </div>
-                </a>
-                <a href="#" class="linkrow" >
+
+                <?php 
+                    //display all active categories
+                    $sql = "SELECT * FROM tbl_category WHERE active='Yes'";
+                    $res = mysqli_query($conn, $sql);
+                    $count = mysqli_num_rows($res);
+
+                    if($count>0)
+                    {
+                        while($row=mysqli_fetch_assoc($res))
+                        {
+                            $id = $row['id'];
+                            $title = $row['title'];
+                            $image_name = $row['image_name'];
+                            ?>
+
+                            <a href="#" class="linkrow">
+                                <div class="catcard shadow bigger">
+                                        <h3 class="card-title"><?php echo $title; ?></h3>
+                                        <?php
+                                            if($image_name=="")
+                                            {
+                                                echo "<div class='error'>Image Not Found</div>";
+                                            }
+                                            else
+                                            {
+                                                ?>
+                                                     <img src="<?php echo SITEURL; ?>images/categories/<?php echo $image_name;?>" alt="<?php echo $title ?>" class="img-responsive cat-img" />
+                                                <?php
+                                            }
+                                        ?>
+                                </div>
+                            </a>
+
+                            <?php
+                        }
+                    }
+                    else
+                    {
+                        echo "<div class='error'>Category Not Found</div>";
+                    }
+                ?>
+
+                
+
+                <!-- <a href="#" class="linkrow" >
                     <div class="catcard shadow bigger">
                         <h3 class="card-title">Lunch</h3>
                         <img src="images/quinoa-fusion-salad-cloud.jpg" alt="lunch" class="img-responsive cat-img" />
@@ -22,7 +61,7 @@
                         <h3 class="card-title">Dinner</h3>
                         <img src="images/galactic-gourmet-steak.jpg" alt="dinner" class="img-responsive cat-img" />
                     </div>
-                </a>
+                </a> -->
             </div>
         </div>
     </section>
