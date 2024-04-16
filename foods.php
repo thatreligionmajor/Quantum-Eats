8d71a0a8d96225d5375fd724ff7fef4959eec7c1
Theresa Barkasy <theresa.barkasy@gmail.com>
2024-04-16 11:11:41 -0400

    file prepped for php

--- foods.php
+++ foods.php
@@ -5,6 +5,11 @@
         <div class="container">
             <h2 class="section-header menu-header text-center">Quantum Laboratory</h2>
             <div class="row">
+
+                <?php
+
+                ?>
+
                 <div class="menucard shadow">
                     <div class="menu-img">
                         <img src="images/spherified-olive-pearls.jpg" alt="spherified olive pearls" class="img-rounded">
@@ -21,7 +26,8 @@
                         </a>
                     </div>
                 </div>
-                <div class="menucard shadow">
+                
+                <!-- <div class="menucard shadow">
                     <div class="menu-img">
                         <img src="images/molecular-forest-sorbet.jpg" alt="molecular forest sorbet" class="img-rounded">
                     </div>
@@ -36,10 +42,10 @@
                               </svg>
                         </a>
                     </div>
-                </div>
+                </div> -->
             </div>
             <div class="row">
-                <div class="menucard shadow">
+                <!-- <div class="menucard shadow">
                     <div class="menu-img">
                         <img src="images/seared-matcha-infused-tofu.jpg" alt="seared matcha-infused tofu" class="img-rounded">
                     </div>
@@ -54,8 +60,8 @@
                             </svg>
                         </a>
                     </div>
-                </div>
-                <div class="menucard shadow">
+                </div> -->
+                <!-- <div class="menucard shadow">
                     <div class="menu-img">
                         <img src="images/truffle-infusion-eruption.jpg" alt="truffle infusion eruption" class="img-rounded">
                     </div>
@@ -70,10 +76,10 @@
                             </svg>
                         </a>
                     </div>
-                </div>
+                </div> -->
             </div>
             <div class="row">
-                <div class="menucard shadow">
+                <!-- <div class="menucard shadow">
                     <div class="menu-img">
                         <img src="images/green-velvet-gazpacho.jpg" alt="green velvet gazpacho" class="img-rounded">
                     </div>
@@ -88,8 +94,8 @@
                             </svg>
                         </a>
                     </div>
-                </div>
-                <div class="menucard shadow">
+                </div> -->
+                <!-- <div class="menucard shadow">
                     <div class="menu-img">
                         <img src="images/deconstructed-tiramisu-sphere.jpg" alt="deconstructed tiramisu sphere" class="img-rounded">
                     </div>
@@ -104,7 +110,7 @@
                               </svg>
                         </a>
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
     </section>
